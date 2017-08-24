92f6ead2d33683d81b4b6d07e8a7c39762f9d2c8
Sri Harsha Gangisetty <hars4646@gmail.com>
2017-08-25 02:57:50 +0530

    Added a cx_Freeze setup script to create Executable on windows; redirect stdout, stderr to log.txt;

---
+++ cx_setup.py
@@ -0,0 +1,24 @@
+from cx_Freeze import setup, Executable
+ 
+# Dependencies are automatically detected, but it might need
+## setup file for cx_freeze to build Windows Executable
+## create executable using command `python cx_setup.py`
+
+
+buildOptions = dict(packages = ["idna","shutil","ltc_scrypt","argparse","dns","gui"], excludes = [], include_files=["lib"])
+ 
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+ 
+executables = [
+    Executable('electrum-xvg', base=base, icon = "./icons/electrum.ico")
+]
+ 
+setup(
+    name='Electrum XVG',
+    version="2.4",
+    description = 'Electrum Wallet for Verge (XVG)',
+    options = dict(build_exe = buildOptions),
+    executables = executables,
+    
+)
