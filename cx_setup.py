bdf15151f8251b3bb5a14d82e868cf0af25587d5
Sri Harsha Gangisetty <hars4646@gmail.com>
2017-09-22 00:23:52 +0530

    Updated version number to 2.4.1 and updated cx_setup.py

--- cx_setup.py
+++ cx_setup.py
@@ -1,13 +1,22 @@
 from cx_Freeze import setup, Executable
+import os,sys
  
 # Dependencies are automatically detected, but it might need
 # setup file for cx_freeze to build Windows Executable
 # create executable using command `python cx_setup.py build`
+script_dir  = os.path.dirname(os.path.realpath(__file__))
 
+is_bundle = getattr(sys, 'frozen', False)
+is_local = not is_bundle and os.path.exists(os.path.join(script_dir, "setup-release.py"))
+is_android = 'ANDROID_DATA' in os.environ
 
+if is_bundle or is_local or is_android:
+    import imp
+    imp.load_module('electrum_xvg', *imp.find_module('lib'))
+
+from electrum_xvg.version import ELECTRUM_VERSION
 buildOptions = dict(packages = ["idna","shutil","ltc_scrypt","argparse","dns","gui","dbhash","dumbdbm"], excludes = [], include_files=["lib"])
  
-import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
  
 executables = [
@@ -16,7 +25,7 @@ executables = [
  
 setup(
     name='Electrum XVG',
-    version="2.4",
+    version=ELECTRUM_VERSION,
     description = 'Electrum Wallet for Verge (XVG)',
     options = dict(build_exe = buildOptions),
     executables = executables,    
