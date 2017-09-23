837eec91f2d295eaf73647e9cfddc88613c5de16
sunerok <justinvforvendetta@users.noreply.github.com>
2017-09-23 15:02:44 -0400

    added electrum-xvg.party to server list

--- lib/network.py
+++ lib/network.py
@@ -19,7 +19,8 @@ DEFAULT_PORTS = {'t':'50001', 's':'50002', 'h':'8081', 'g':'8082'}
 
 DEFAULT_SERVERS = {
     'electrum-verge.xyz':{'t':'50001', 's':'50002'},
-	'electrum-xvg.stream':{'t':'50001', 's':'50002'},
+    'electrum-xvg.stream':{'t':'50001', 's':'50002'},
+    'electrum-xvg.party':{'t':'50001', 's':'50002'},
 
 }
 
